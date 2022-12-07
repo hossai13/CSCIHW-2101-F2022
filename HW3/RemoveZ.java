7bfe5a9243f9e29bccf407c766b440fe7c7ec255
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 02:29:41 -0500

    finished

--- HW3/RemoveZ.java
+++ HW3/RemoveZ.java
@@ -2,13 +2,6 @@ package HW3;
 import java.util.Scanner;
 
 public class RemoveZ {
-    //Create a method Given a string, return the string where all of the "z"
-    //Have been removed. Except do not remove a z at the start
-    //or end
-    //Ex.
-    //stringZ("zHelloz") -> "zHelloz"
-    //stringZ("nozthaznks") -> "nothanks"
-    //stringZ("xksiazdjaasldzsajzasdz") -> "xksiadjaasldsajasdz"
     static Scanner keyboard = new Scanner(System.in);
 
     public static String stringZ(){
@@ -16,7 +9,7 @@ public class RemoveZ {
         String str = keyboard.nextLine();
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i)=='z'&& i!=0 && i!=str.length()-1)
-                str = str.substring(0,i) + str.substring(0+i);
+                str = str.substring(0,i) + str.substring(i+i);
         }
         return str;
     }
