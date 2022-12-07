06469d9ff4dbc80308987dd17d58b3148b12e087
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 02:32:19 -0500

    finished

--- HW3/SumUp.java
+++ HW3/SumUp.java
@@ -2,32 +2,6 @@ package HW3;
 import java.util.Scanner;
 
 public class SumUp {
-    //Create a method that contains a while loop that allows for
-    //The user to input numbers until the number 0 is entered. Each time a number is 
-    //entered the total will be summed and then prompted for a second number. 
-    //NOTE: I require the use of scanner here to get each integer from the user
-    //Assume the numbers entered are integers
-    //Also note that the method is void meaning you will not need to return anything
-    //It will need to sysout the numbers however
-    //Below is a sample output what I would like
-    // I will add up the numbers you give me....
-    // Number: 12
-    // The total so far is 12.
-    // Number: 2
-    // The total so far is 14.
-    // Number: 3
-    // The total so far is 17.
-    // Number: 4
-    // The total so far is 21.
-    // Number: 1
-    // The total so far is 22.
-    // Number: 2
-    // The total so far is 24.
-    // Number: 3
-    // The total so far is 27.
-    // Number: 0
-    // TOTAL ENDED --- The total is 27.
-
     static Scanner keyboard = new Scanner(System.in);
 
      public static void sums(){
@@ -41,8 +15,8 @@ public class SumUp {
              if (nnew == 0) {
                 System.out.println("TOTAL ENDED --- The total is " + n);
                 break;
-             }
-    }
+            }
+        }
     }
 }
 
