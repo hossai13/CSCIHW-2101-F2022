acfe4e4af311951d63f7c8a3ad38b07ea57755a8
BuildTools <unconfigured@null.spigotmc.org>
2022-12-16 18:22:56 -0500

    finished

--- Project1/BankAccount.java
+++ Project1/BankAccount.java
@@ -2,27 +2,12 @@ package Project1;
 
 import java.util.Scanner;
 
-/*
- * This is a Java Doc
- * This is a multi-line comment
- *  
- * Our BankAccount class will represent a bank account
- * It will have a balance and a name
- * It will have methods to deposit and withdraw money
- * It will have a method to print the balance
- * it will have a menu that the user can interact with
- * 
- * 
- */
-
 public class BankAccount {
     private double balance;
     private String name;
     private double interestRate = 0.01;
-    private int period = 4; // quarterly
+    private int period = 4;
 
-
-    // Constructor
     public BankAccount(String name, double balance) {
         this.name = name;
         this.balance = balance;
@@ -35,31 +20,20 @@ public class BankAccount {
         this.balance = balance;
         this.name = "No Name";
     }
-    // Default Constructor
-    // Make a BankAccount without Specifying any parameters
+   
     public BankAccount(){
         this.balance = 0;
         this.name = "No Name";
     }
 
-    // Create getters and setters for balance
     public double getBalance() {
         return balance;
     }
 
-    // Create a method for printing the balance as a String
     public String printBalance() {
         return "Your Balance is: $" + balance;
     }
 
-    // public void setBalance(double balance) {
-    //     this.balance = balance;
-    // }
-    // Set balance is flawed in that we would never
-    // be able to actually set the balance. Instead we need 
-    // to create methods to deposit and withdraw money
-
-    // Create getters and setters for name
     public String getName() {
         return name;
     }
@@ -67,46 +41,17 @@ public class BankAccount {
         this.name = name;
     }
     
-
-    // Create a method to deposit money
     public void deposit(double amount) {
         this.balance += amount;
-        // above is the same as this.balance = this.balance + amount;
-        // balance is 10,000
-        // amount  is 2,000
-        // balance = balance + amount
-        // 10000 = 10000 + 2000
     }
 
-    // Create a method to withdraw money
     public void withdraw(double amount) {
         this.balance -= amount;
-        // above is the same as this.balance = this.balance - amount;
-        // balance is 10,000
-        // amount  is 2,000
-        // balance = balance - amount
-        // 10000 = 10000 - 2000
     }
 
-    // Create a method that will subtract a monthly fee from the balance
-    // This method will take a double as an argument
-    // This method will subtract the fee from the balance
     public void monthlyFee(double fee) {
         withdraw(fee);
     }
-
-
-    // Create a method that will calculate the interest on the balance using compound interest
-    // P(1 + R/n)^(nt) - P 
-    // P = Principal
-    // R = Rate
-    // n = number of times interest is compounded per year (annum)
-    // t = number of years
-    // This method will take a double as an argument
-    // This method will return a double
-
-    // P can be this.balance or getBalance() if you would like to use a getter
-
 
     public void calcInterest(double principal, int time, double rate, int annum){
         double amount = principal * Math.pow(1+ (rate/annum), annum*time);
@@ -120,12 +65,17 @@ public class BankAccount {
         System.out.println("Amount after " + time + " years: $" + roundedamount);
     }
 
-
-
-
-
-    // Create a method that will print a menu to the user
-    // this is called a helper method
+    public void calcSimpleInterest(double principal, int time, double rate){
+        double amount = (principal * rate * time)/100;
+        double roundedamount = Math.round(amount * 100.0) / 100.0;
+        double simpleinterest = amount;
+        simpleinterest = Math.round(simpleinterest * 100.0) / 100.0;
+        System.out.println("Current Rate is: "+rate+"%");
+        System.out.println("Selected Time is: "+time+" years");
+        System.out.println("Simple Interest after " + time + " years: $" + simpleinterest);
+        System.out.println("Amount after " + time + " years: $" + roundedamount);
+    }
+    
     public static int menu(){
         int choice;
         Scanner input = new Scanner(System.in);
@@ -134,53 +84,126 @@ public class BankAccount {
         System.out.println("1. Deposit Money");
         System.out.println("2. Withdraw Money");
         System.out.println("3. Print Balance");
-        System.out.println("4. Calculate Interest");
+        System.out.println("4. Calculate Compound Interest");
+        System.out.println("5. Calculate Simple Interest");
+        System.out.println("6. Transfer Money");
+        System.out.println("7. Savings Account");
         System.out.println("0. Exit");
-        System.out.println("Please enter your choice: ");
+        System.out.print("Please enter your choice: ");
         choice = input.nextInt();
         return choice;
     }
-    // Create a method that will interact with the user based on their choice from the menu
-    // This method will take a BankAccount object as an argument
-    public static void interact(BankAccount account){
-        // get the choice from the menu
-        int choice = menu();
-        // create a scanner object
-        Scanner input = new Scanner(System.in);
-        // use that choice and run the method associated with that choice
-        // in class we learned about recursion and how to use it
-        // we will use recursion to keep the menu running until the user chooses to exit
-        // This is outside of the scope of this class but it is worth knowing about
 
+    public void overdraft(double amount){
+        if (balance - amount < 0){
+            System.out.println("You have insufficient funds to withdraw this amount");
+            System.out.println("Would you like to withdraw the amount and be charged a fee of $35?");
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+            Scanner input = new Scanner(System.in);
+            int choice = input.nextInt();
+            if (choice == 1){
+                balance -= amount;
+                balance -= 35;
+                System.out.println("You have been charged a fee of $35");
+                System.out.println("Your new balance is: $" + balance);
+            } else if (choice == 2){
+                System.out.println("Your balance is: $" + balance);
+            } else {
+                System.out.println("Invalid Choice");
+            }
+        } else {
+            balance -= amount;
+            System.out.println("Your new balance is: $" + balance);
+        }
+    }
+
+    public static void transfer(BankAccount gregChecking, BankAccount mdChecking){
+        System.out.println("How much would you like to transfer?");
+        Scanner input = new Scanner(System.in);
+        double amount = input.nextDouble();
+        gregChecking.overdraft(amount);
+        mdChecking.deposit(amount);
+        System.out.println("Account 2's new balance is: $" + mdChecking.getBalance());
+    }
+
+    public static void interact(BankAccount account, BankAccount account2){
+        int choice = menu();
+        Scanner input = new Scanner(System.in);
         if (choice == 1) {
             System.out.println("How much would you like to deposit?");
             double amount = input.nextDouble();
             account.deposit(amount);
             System.out.println(account.printBalance());
-            // interact(account);
+          
         } else if (choice == 2) {
             System.out.println("How much would you like to withdraw?");
             double amount = input.nextDouble();
-            account.withdraw(amount);
             System.out.println(account.printBalance());
-            // interact(account);
+            account.overdraft(amount);
+    
         } else if (choice == 3) {
             System.out.println(account.printBalance());
-            // interact(account);
+            
         } else if (choice == 4) {
             System.out.println("What is the time in years to invest (Whole Numbers Only)?");
             int time = input.nextInt();
-            // double principal, int time, double rate, int period
             account.calcInterest(account.getBalance(), time, account.interestRate, account.period);
-
             System.out.println("... Thank you for using the Bank Account Menu. Have a nice day! ...");
 
-        }else if (choice == 0) {
+        } else if (choice == 5) {
+            System.out.println("What is the time in years to invest (Whole Numbers Only)?");
+            int time = input.nextInt();
+            account.calcSimpleInterest(account.getBalance(), time, account.interestRate);
+            System.out.println("... Thank you for using the Bank Account Menu. Have a nice day! ...");
+        } else if (choice == 6) {
+            System.out.println("Would you like to transfer money to Account 2?");
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+            int choice2 = input.nextInt();
+            if (choice2 == 1){
+                transfer(account, account2);
+            } else if (choice2 == 2){
+                System.out.println("... Thank you for using the Bank Account Menu. Have a nice day! ...");
+            } else {
+                System.out.println("Invalid Choice");
+            }
+            System.out.println("... Thank you for using the Bank Account Menu. Have a nice day! ...");
+        } else if (choice == 7) {
+            savings(account);
+        } else if (choice == 0) {
             System.out.println("Thank you for using the Bank Account Menu");
-        } else { // this would catch 69420
+        } else { 
             System.out.println("Invalid Choice");
-            // interact(account);
+            
         }
 
+    }
+    public static void savings(BankAccount account) {
+        System.out.println("\nWould you like to create a savings account?");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        if (choice == 1) {
+            System.out.println("How much would you like to deposit?");
+            double amount = input.nextDouble();
+            account.deposit(amount);
+            System.out.println("Would you like to create a savings account with compound interest or simple interest?");
+            System.out.println("1. Compound Interest");
+            System.out.println("2. Simple Interest");
+            int choice2 = input.nextInt();
+            if (choice2 == 1) {
+                account.calcInterest(account.getBalance(), 1, 0.0001, 4);
+            } else if (choice2 == 2) {
+                account.calcSimpleInterest(account.getBalance(), 1, 0.001);
+            } else {
+                System.out.println("Invalid Choice");
+            }
+        } else if (choice == 2) {
+            System.out.println("Thank you for using the Bank Account Menu");
+        } else {
+            System.out.println("Invalid Choice");
+        }
     }
 }
--- Project1/BankAccountTester.java
+++ Project1/BankAccountTester.java
@@ -2,9 +2,12 @@ package Project1;
 
 public class BankAccountTester {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Bank Account Tester");
-        // Example Bank Account used for Transfer Methods
+        System.out.println("Welcome to the Bank Account Tester\n ");
+        
         BankAccount gregChecking = new BankAccount(20000);
+        BankAccount mdChecking = new BankAccount(10000);
+
+        BankAccount.interact(gregChecking, mdChecking);
 
     }
 }
