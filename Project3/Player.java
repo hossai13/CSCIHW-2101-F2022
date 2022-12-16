44cdb65fa6c9fdcd1eaea59f6c33957a7ae2762e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-16 12:52:36 -0500

    project3

---
+++ Project3/Player.java
@@ -0,0 +1,15 @@
+package Project3;
+
+public class Player {
+    private int lives;
+
+    // make a Player object with no parameter (default lives = 5)
+
+    // make a player object with lives parameter
+
+    // make a method to get the lives 
+    // return lives (int)
+
+    // make a death method to subtract a life from the lives total (void)
+    
+}
