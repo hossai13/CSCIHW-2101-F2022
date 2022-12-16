f3f36a009afebad4d9def09bf354f44d16b488c0
BuildTools <unconfigured@null.spigotmc.org>
2022-12-16 01:46:58 -0500

    finished

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -3,26 +3,6 @@ import java.util.Scanner;
 
 public class Arrays {
 
-    // CONVERT THE SUMS METHOD FROM HW3 TO ADD EACH VALUE TO AN ARRAY AND THEN OUTPUT THE 
-    // ARRAY AND THE TOTAL.
-    // SEE HW3 FOR SUMS METHOD
-    // If you want to use an ArrayList for the inputs to be dynamic that is okay
-    // Otherwise if you are using a regular array, assume the size is 10 total. 
-    // EXAMPLE 
-    // I will add up the numbers you give me....
-    // Number: 12
-    // The total so far is 12.
-    // Number: 2
-    // The total so far is 14.
-    // Number: 3
-    // The total so far is 17.
-    // Number: 4
-    // The total so far is 21.
-    // Number: 1
-    // The total so far is 22.
-    // Number: 0
-    // Total: 22
-    // Array: {12,2,3,4,1,0}
     public static void Arraysums(){
         Scanner keyboard = new Scanner(System.in);
         int[] array = new int[10];
@@ -47,23 +27,8 @@ public class Arrays {
                 System.out.print(",");
             }
         }
-        System.out.println("}");
+        System.out.print("}");
     }
-
-    // Create a method that will brute force a password EX.
-    // bruteForce("ZZZZ")
-    // The program should then guess each char of the string and compare it to see if it is equal
-    // to the index of the alphabet char array below
-    // Output Example:
-    // Z
-    // ZZ
-    // ZZZ
-    // ZZZZ
-
-    // Hint 1: using a char array for the password is helpful here 
-    // Hint 2: using the method .toCharArray is a way to convert the password parameter to
-    // a char array as well
-    // From there creating for and while loops to compare the values is trivial
 
     public static String bruteForce(String password){
         String answer = "";
@@ -90,20 +55,36 @@ public class Arrays {
 
     // Hint 1: a temp Array is a helpful way to store variables for comparing 
     // Hint 2: 2 nested For loops should be all thats needed for the swapping logic
-    public static int[] sorter(int[] nums){
-        int [] sortedArray = new int[nums.length];  
+    public static int[] sorter(int[] nums){       
+        int[] sortedArray = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i] > nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }  
+            System.out.print(nums[i]);
+            if(i != nums.length-1){
+                System.out.print(", ");
+            }
+         }
+         
         return sortedArray;
     }
+
 
     public static void main(String[] args) {
         // Tester main method for your methods
         System.out.println("Hello World!\n");
         Arraysums();
         System.out.println("\n");
-        System.err.println(bruteForce("sEmIsOvEr!")+"\n");
-        System.out.println(sorter(new int[]{1,56,23,31}));
+        System.out.println(bruteForce("sEmIsOvEr!"));
 
+        System.out.print("[");
+        int[] nums = {4,10,3,5,7,3,6,8};
+        sorter(nums);
+        System.out.println("]");
     }
-
-    
 }
